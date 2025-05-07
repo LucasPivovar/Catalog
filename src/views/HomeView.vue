518f1dc39270174910e1f21555ba271506c1a868
LucasPivovar <lucascaetano956@gmail.com>
2025-05-07 19:13:34 -0300

    mudanças no profile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,9 @@
         ></catalog-cards>     
       </div>
     </div>      
-  </body>
+  </body> 
+  <a href="/access" class="access">Acessar</a>
+  <a href="/dashboard" class="dashboard">Dashboard</a>
 </template>  
 
 <script>
@@ -74,3 +76,54 @@ export default {
   } 
 } 
 </script>  
+
+<style scoped>
+  .main{
+    font-family: "inter";   
+    background: var(--bg-main);
+  }    
+  
+  .content{     
+    width: 100%;     
+    min-height: 100vh;     
+    max-width: 1400px;   
+  }    
+  
+  header{     
+    display: flex;     
+    color: var(--text-primary);
+    align-items: center;     
+    justify-content: space-between;     
+    margin-top: 1rem;     
+    padding: 0 35px;     
+    width: 100%;   
+  }    
+  
+  header h1{     
+    font-weight: 400;     
+    font-size: 1.6rem;   
+  }    
+  
+  header strong{     
+    font-weight: 400;     
+    color: var(--text-accent);
+  }    
+  
+  header p {     
+    font-size: 1.1rem;     
+    font-weight: 300;     
+    display: flex;     
+    justify-content: center;     
+    flex-direction: row;     
+    align-items: center;     
+    gap: 5px;   
+  }    
+  
+  header p span{     
+    background: transparent;
+  }    
+  
+  header p span img{     
+    width: 25px;   
+  } 
+</style>
